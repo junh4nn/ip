c4a67d069dfd64f3c8dee734c2111094735854aa
junh4nn <junh4nn@gmail.com>
2026-08-21 06:25:50 +0800

    add greeting

--- src/main/java/JimBO.java
+++ src/main/java/JimBO.java
@@ -1,4 +1,6 @@
 public class JimBO {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = "   ___   _____  ___  ___ ______   _____ \n"
                 + "  |_  | |_   _| |  \\/  | | ___ \\ |  _  |\n"
@@ -6,6 +8,14 @@ public class JimBO {
                 + "    | |   | |   | |\\/| | | ___ \\ | | | |\n"
                 + "/\\__/ /  _| |_  | |  | | | |_/ / \\ \\_/ /\n"
                 + "\\____/   \\___/  \\_|  |_/ \\____/   \\___/ \n";
+
+        System.out.println(LINE);
         System.out.println(banner);
+        System.out.println("Hello! I'm JimBO.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }

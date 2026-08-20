9f894e2fec5a9ab86eb5e594c7bc5ecac2e4531d
junh4nn <junh4nn@gmail.com>
2026-08-21 06:36:15 +0800

    added echo feature

--- src/main/java/JimBO.java
+++ src/main/java/JimBO.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class JimBO {
     private static final String LINE = "____________________________________________________________";
 
@@ -15,7 +17,17 @@ public class JimBO {
         System.out.println("What can I do for you?");
         System.out.println(LINE);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+            System.out.println(command);
+            System.out.println(LINE);
+        }
+        scanner.close();
     }
 }

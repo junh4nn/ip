7329dd9f66bb665316c59d5bf33a9357d995f583
junh4nn <junh4nn@gmail.com>
2026-08-21 09:34:46 +0800

    improved list feature and added mark feature

--- src/main/java/JimBO.java
+++ src/main/java/JimBO.java
@@ -18,6 +18,7 @@ public class JimBO {
         System.out.println(LINE);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -28,9 +29,17 @@ public class JimBO {
                 System.out.println(LINE);
                 break;
             } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
                 }
+                System.out.println(LINE);
+            } else if (command.startsWith("mark ")) {
+                int index = Integer.parseInt(command.substring(5)) - 1;
+                isDone[index] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + tasks[index]);
                 System.out.println(LINE);
             } else {
                 tasks[taskCount] = command;

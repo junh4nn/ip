6a95d5f80caf8e9619bfe0233b5c60d5621b197f
junh4nn <junh4nn@gmail.com>
2026-08-21 09:37:30 +0800

    added unmark feature

--- src/main/java/JimBO.java
+++ src/main/java/JimBO.java
@@ -41,6 +41,12 @@ public class JimBO {
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  [X] " + tasks[index]);
                 System.out.println(LINE);
+            } else if (command.startsWith("unmark ")) {
+                int index = Integer.parseInt(command.substring(7)) - 1;
+                isDone[index] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + tasks[index]);
+                System.out.println(LINE);
             } else {
                 tasks[taskCount] = command;
                 taskCount++;
